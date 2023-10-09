339a516ef2fd8feaea8cd07a7f2bcce34e570da8
qyang10 <68740780+qyang10@users.noreply.github.com>
2023-10-09 14:36:27 -0400

    Add files via upload

---
+++ Two Sample MR scripts.R
@@ -0,0 +1,16 @@
+install.packages("remotes")
+library(remotes)
+install_github("MRCIEU/TwoSampleMR")
+
+
+# Get instruments
+exposure_dat <- extract_instruments("ieu-a-2")
+
+# Get effects of instruments on outcome
+outcome_dat <- extract_outcome_data(snps=exposure_dat$SNP, outcomes = "ieu-a-7")
+
+# Harmonise the exposure and outcome data
+dat <- harmonise_data(exposure_dat, outcome_dat)
+
+# Perform MR
+res <- mr(dat)
